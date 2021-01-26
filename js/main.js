1c513101f3e7186b4120258945d1194045562fee
EPeng <erpengic@gmail.com>
2021-01-25 19:48:42 -0600

    added grid size slider and etch functionality

--- js/main.js
+++ js/main.js
@@ -1,27 +1,51 @@
-let x_grid = 16
-let y_grid = 16
+let gridSize = document.querySelector('#gridSize')
 const parentGrid = document.getElementById('parent-grid')
 const resetGrid = document.getElementById('reset-button')
 
-createGrid()
-const squares = document.querySelectorAll('.square')
-Array.from(squares).forEach(square => square.addEventListener('mouseenter', function(){
-    square.classList.add('squareGrey')
-}))
-
+gridSize.addEventListener('input', updateSize)
 resetGrid.addEventListener('click', clearGrid)
 
+createGrid()
+// const squares = document.querySelectorAll('.square')
+// Array.from(squares).forEach(square => square.addEventListener('mouseenter', function(){
+//     square.classList.add('squareGrey')
+// }))
+
+//Generates number of squares (gridLength x gridLength) based on slider input and places into div #parent-grid
 function createGrid(){
-    for(let x = 0; x<x_grid; x++){
-        for(let y = 0; y<y_grid; y++){
+    let gridLength = gridSize.value
+    for(let x = 0; x<gridLength; x++){
+        for(let y = 0; y<gridLength; y++){
             let newSquare = document.createElement('div')
             newSquare.classList.add('square')
             parentGrid.appendChild(newSquare)
         }
     }
+    setEtch()
+}
+
+//Sets the CSS variable --grid-size to the input value of the slider and prints it to the page
+function updateSize(){
+    deleteGrid(parentGrid)
+    document.getElementById('parent-grid').style.setProperty('--grid-size', gridSize.value)
+    document.getElementById('sizeVar').innerHTML = gridSize.value
+    createGrid()
+    
 }
 
 function clearGrid(){
-    squares.forEach(square => square.classList.remove('squareGrey'))
+    document.querySelectorAll('.square').forEach(square => square.classList.remove('squareGrey'))
 }
 
+function deleteGrid(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild)
+    }
+}
+
+function setEtch(){
+    const squares = document.querySelectorAll('.square')
+    Array.from(squares).forEach(square => square.addEventListener('mouseenter', function(){
+        square.classList.add('squareGrey')
+    })) 
+}
